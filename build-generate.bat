04ef0f2595d416a6bf773aea14b864566a4e8aba
jamesmcgill <jamesmcgill@users.noreply.github.com>
2019-10-05 12:02:55 +0200

    build system: update batch scripts to run under VS 2019

--- build-generate.bat
+++ build-generate.bat
@@ -1,31 +1,53 @@
 :: Generates CMake Cache
 :: Deletes any previous cache and build files
-:: If the environment variable VCPKG_PATH is set, vcpkg's toolchain will be used
 
 @echo off
 set BASE_PATH=%~dp0
 set BUILD_PATH=%BASE_PATH%build\
+set GENERATOR="Visual Studio 16 2019"
 
-IF NOT EXIST %BUILD_PATH% GOTO MissingBuildDir
-echo "Deleting Build Path..."
+:: This script needs vcpkg and needs to know where it is installed
+if [%VCPKG_PATH%] == [] goto MISSING_VCPKG
+
+:: Delete any existing cache (by deleting entire build directory)
+if not exist %BUILD_PATH% goto MISSING_BUILD_DIR
+echo.
+echo Deleting Build Path...
 rmdir /S /Q %BUILD_PATH%
-:MissingBuildDir
-IF NOT EXIST %BUILD_PATH% mkdir %BUILD_PATH%
+:MISSING_BUILD_DIR
+if not exist %BUILD_PATH% mkdir %BUILD_PATH%
 
+
+:: Generate new cache
 pushd %BUILD_PATH%
-
-IF NOT DEFINED VCPKG_PATH goto NoVcpkg
-echo Using VcPkg toolchain located at %VCPKG_PATH% 
-cmake -G "Visual Studio 15 2017" .. "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake"
-goto FinishCMake
-
-:NoVcpkg
-echo VcPkg not found. Continuing without. 
-cmake -G "Visual Studio 15 2017" ..
-
-:FinishCMake
+echo.
+echo Using generator: %GENERATOR%
+call cmake -A x64 -G %GENERATOR% .. -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake && (
 popd
+goto SUCCESS
+) || (
+del CMakeCache.txt
+)
 
+:: CMake failed if we reached this point
+popd
+echo.
+echo.
+echo If CMake errors are related to not finding any supported MSVC compiler
+echo Check you are in the vsdevcmd shell
+echo Check this file (build-generate.bat) supports your vsdevcmd compiler version (add if necessary)
+exit
+
+
+:MISSING_VCPKG
+echo.
+echo This script assumes vcpkg is used to provide build dependencies
+echo (https://github.com/Microsoft/vcpkg)
+echo - Please install vcpkg and create an environment variable VCPKG_PATH pointing to it
+echo.
+exit
+
+:SUCCESS
 echo[
 echo CMake Cache Generation Done!
 echo ----------------------------
--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
 @echo off
 ::##############################################################################
 :: Incrementally build and run tests
-:: Expects build-generate.bat to be run beforehand
 ::##############################################################################
 
 :: Configuration variables. You may edit these.
 set BUILD_TARGET="Release"
+set EXE_NAME="distribution_viz"
+
 
 
 ::##############################################################################
@@ -14,16 +15,21 @@ set BUILD_TARGET="Release"
 set BASE_PATH=%~dp0
 set BUILD_PATH=%BASE_PATH%build\
 
-IF EXIST %BUILD_PATH%CMakeCache.txt GOTO Build
+:: Generate the project cache (if required)
+if exist %BUILD_PATH%CMakeCache.txt goto SKIP_CMAKE
+echo.
 echo Missing CMakeCache.txt. Running build-generate.bat
 call %BASE_PATH%build-generate.bat
+:SKIP_CMAKE
 
-:Build
+:: Build and run our code
 pushd %BUILD_PATH%
-cmake --build . --config %BUILD_TARGET%
+echo.
+call cmake --build . --config %BUILD_TARGET% && (
+.\%BUILD_TARGET%\%EXE_NAME%.exe
+)
 popd
 
-echo[
+echo.
 echo Done!
 echo -----
-:End
